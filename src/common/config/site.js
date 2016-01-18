4ce858494bb7b141f73d6712059d794bdc0df828
SinalVee <sinalvee@gmail.com>
2016-01-18 18:29:47 +0800

    Move site description to config file

---
+++ src/common/config/site.js
@@ -0,0 +1,11 @@
+'use strict';
+/**
+ * site config
+ */
+export default {
+  //key: value
+  name: 'SinalVee\'s Blog',
+  description: 'Thoughts, stories and ideas.',
+  signature: 'Just do, don\'t BB.',
+  githubUrl: 'https://github.com/SinalVee'
+};
